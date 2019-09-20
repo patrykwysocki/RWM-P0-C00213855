6eef9fe792f38ff0654aac38e72cb3802cf7db68
patrykwysocki <patrykwysocki>
2019-09-20 22:05:39 +0100

    running total filter and tests implemented

---
+++ RWM-P0-C00213855/Assets/Tests/RunningTotalFilterTest.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class RunningTotalFilterTest
+    {
+        [Test]
+        public void RunningTotalSimple()
+        {
+            //Assert.IsTrue(false);
+
+            int[] input = { 4, 2, 8, 3, 9, 4, 10, 5, 11, 6 };
+            int[] output = RunningTotalFilter.RunningTotals(input);
+            int[] expected = { 4, 6, 14, 17, 26, 30, 40, 45, 56, 62 };
+
+            //foreach (var o in output)
+            //{
+            //    Debug.Log(o);
+            //}
+
+            CollectionAssert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void RunningTotalComplex()
+        {
+            //Assert.IsTrue(false);
+
+            int[] input = {0,-11,9999,11};
+            int[] output = RunningTotalFilter.RunningTotals(input);
+            int[] expected = { 0,-11,9988,9999 };
+
+            //foreach (var o in output)
+            //{
+            //    Debug.Log(o);
+            //}
+
+            CollectionAssert.AreEqual(expected, output);
+        }
+    }
+}
